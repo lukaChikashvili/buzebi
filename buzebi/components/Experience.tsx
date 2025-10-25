bc6cd7555e662d2385b83f7b8f05e18e3dc037bf
luka chikashvili <lukachikashvili2020@gmail.com>
2025-10-26 00:22:38 +0400

    remove 3d model parts

--- buzebi/components/Experience.tsx
+++ buzebi/components/Experience.tsx
@@ -1,9 +1,26 @@
 import { useGLTF } from "@react-three/drei"
+import { useEffect } from "react";
+import * as THREE from 'three'
 
 const Experience = () => {
   
     //  room model
     const room = useGLTF('./old_room.glb');
+
+    useEffect(() => {
+       room.scene.traverse((child) => {
+            if((child as THREE.Mesh).isMesh) {
+                if(child.name === "Cylinder001_01_-_Default_0") {
+                    child.visible = false;
+                }else if(child.name === "Cylinder002_01_-_Default_0") {
+                    child.visible = false;
+                }else if(child.name === "Sphere001__0") {
+                    child.visible = false;
+                }
+              
+            }
+       })
+    }, [room])
 
 
   return (
