6db40df35b23eb898a75bba756bd359d1f48421c
luka chikashvili <lukachikashvili2020@gmail.com>
2026-03-05 10:18:22 +0400

    add wall design

--- buzebi/components/Cinema.jsx
+++ buzebi/components/Cinema.jsx
@@ -9,9 +9,14 @@ const Cinema = ({ screenProps }) => {
     const wood = useTexture('./wood2.jpg');
     const wall = useTexture('./wall1.jpg');
     const frame = useTexture('./frame.jpg');
+    const gold = useTexture('./gold.avif');
+    const cloth = useTexture('./cloth.jpg');
+
+
 
     //  models
     const sofa = useGLTF('./sofa.glb');
+
     const plant = useGLTF('./plant.glb');
 
 
@@ -33,14 +38,16 @@ const Cinema = ({ screenProps }) => {
 
      <rectAreaLight
         width={10}
-        height={5}
+        height={50}
         intensity={2}
-        color={"#ffffff"} 
+        color="blue"
         position={[0, 2.5, -3.9]}
         rotation={[0, Math.PI, 0]}
       />
 
-<pointLight position={[-100, 8.8, 0]} intensity={10} color="#ffaa55" distance={10} />
+<pointLight position={[-100, 8.8, 0]} intensity={10} color="blue" distance={10} />
+
+
  
       <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
         <planeGeometry args={[12, 8, 500, 500]} />
@@ -70,9 +77,31 @@ const Cinema = ({ screenProps }) => {
       </mesh>
 
       <primitive object={sofa.scene} scale = {0.25} rotation = {[0, 1.6, 0]} />
-      <primitive object={plant.scene} scale = {2} position = {[-5, 0, -3.3]} />
+      <primitive object={plant.scene} scale = {2}  position = {[-5, 0, -3.3]} />
+      <primitive object={plant.scene.clone()} scale = {2} position = {[5, 0, -3.3]} />
 
-      
+      <mesh position = {[-6.04, 3.5, -3]}  rotation={[0, Math.PI / 2, 0]}>
+          <boxGeometry args = {[1.1, 3, 0.1] }/>
+          <meshBasicMaterial  color = "gray" />
+      </mesh>
+
+      <mesh position = {[6.04, 3.5, -3]}  rotation={[0, Math.PI / 2, 0]}>
+          <boxGeometry args = {[1.1, 3, 0.1] }/>
+          <meshBasicMaterial  color = "gray" />
+      </mesh>
+
+      <mesh position = {[-6.04, 3.5, -3]}  rotation={[0, Math.PI / 2, 0]}>
+          <boxGeometry args = {[1, 2.8, 0.1] }/>
+          <meshBasicMaterial map = {cloth} color = "#1C4D8D"  />
+      </mesh>
+
+      <mesh position = {[6, 3.5, -3]}  rotation={[0, Math.PI / 2, 0]}>
+          <boxGeometry args = {[1, 2.8, 0.1] }/>
+          <meshBasicMaterial map = {cloth} color = "#1C4D8D"  />
+      </mesh>
+
+
+    
       <Screen  {...screenProps}  />
     </group>
   );
