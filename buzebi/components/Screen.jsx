24c7e5045902229bb4befe80cee59dbb819f2b16
luka chikashvili <lukachikashvili2020@gmail.com>
2026-03-05 10:52:11 +0400

    add full screen camera movement

--- buzebi/components/Screen.jsx
+++ buzebi/components/Screen.jsx
@@ -1,8 +1,26 @@
-import { useFrame } from "@react-three/fiber";
-import React, { forwardRef, useEffect, useMemo } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import React, { forwardRef, useEffect, useMemo, useState } from "react";
 import * as THREE from "three";
+import gsap from 'gsap'
 
 const Screen = forwardRef(({ position, size = [12, 6.2], movieSrc }, ref) => {
+
+  const { camera } = useThree();
+
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  // full screen
+  const fullScreen = () => {
+    setIsFullScreen(!isFullScreen);
+
+    gsap.to(camera.position, {
+      z: isFullScreen ? 10 : -1, 
+      duration: 1,
+      ease: "power2.out"
+    });
+  }
+
+  const currentSize = isFullScreen ? [10.5, 5] : [10.7, 4.2];
 
   const { video, videoTexture } = useMemo(() => {
     if (!movieSrc) return { video: null, videoTexture: null };
@@ -44,8 +62,8 @@ const Screen = forwardRef(({ position, size = [12, 6.2], movieSrc }, ref) => {
   if (!videoTexture) return null;
 
   return (
-    <mesh ref={ref} position={position}>
-      <planeGeometry args={size} />
+    <mesh ref={ref} position={position} onClick = {fullScreen}>
+      <planeGeometry key={isFullScreen.toString()} args={currentSize} />
       <meshBasicMaterial map={videoTexture} toneMapped={false} />
     </mesh>
   );
