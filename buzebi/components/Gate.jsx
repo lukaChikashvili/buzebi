fa92ab8d492a0c70d6db0df01f6b0d72d09eb23b
luka chikashvili <lukachikashvili2020@gmail.com>
2026-03-11 00:40:00 +0400

    add gate and walls

--- buzebi/components/Gate.jsx
+++ buzebi/components/Gate.jsx
@@ -7,8 +7,19 @@ const Gate = () => {
   return (
  <>
     
- <primitive object={gate.scene} />
+ <primitive object={gate.scene} scale = {0.4} rotation = {[0, 1.5, 0]} position = {[-2, -5, 120]} />
  
+ <mesh position={[-20, 10, 145]} rotation={[0, 1.5, 0]}>
+        <boxGeometry args={[30, 16, 5]} />
+        <meshStandardMaterial color="#b8b5b0" />
+      </mesh>
+
+      <mesh position={[8.5, 10, 145]} rotation={[0, 1.5, 0]}>
+        <boxGeometry args={[30, 16, 5]} />
+        <meshStandardMaterial color="#b8b5b0" />
+      </mesh>
+
+
  </>
   )
 }
