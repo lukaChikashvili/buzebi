a89030982dd01809848d4ea156eb6d2db19fa4a3
luka chikashvili <lukachikashvili2020@gmail.com>
2026-03-11 00:51:57 +0400

    add lamps

--- buzebi/app/page.jsx
+++ buzebi/app/page.jsx
@@ -134,7 +134,7 @@ const audioRef = useRef(null);
         
         <Physics gravity={[0, -9.81, 0]}>
        <Menu isOpen={showMenu} />
-   
+   <OrbitControls />
        <Experience />
        </Physics>
        <Lights />
--- buzebi/components/Gate.jsx
+++ buzebi/components/Gate.jsx
@@ -3,21 +3,26 @@ import React from 'react'
 
 const Gate = () => {
       const gate = useGLTF('./gate.glb');
+      const lamp = useGLTF('./lamp.glb');
+
 
   return (
  <>
     
  <primitive object={gate.scene} scale = {0.4} rotation = {[0, 1.5, 0]} position = {[-2, -5, 120]} />
  
- <mesh position={[-20, 10, 145]} rotation={[0, 1.5, 0]}>
-        <boxGeometry args={[30, 16, 5]} />
+ <mesh position={[-22, 10, 145]} rotation={[0, 1.5, 0]}>
+        <boxGeometry args={[30, 20, 5]} />
         <meshStandardMaterial color="#b8b5b0" />
       </mesh>
 
-      <mesh position={[8.5, 10, 145]} rotation={[0, 1.5, 0]}>
-        <boxGeometry args={[30, 16, 5]} />
+      <mesh position={[11.5, 10, 145]} rotation={[0, 1.5, 0]}>
+        <boxGeometry args={[30, 20, 5]} />
         <meshStandardMaterial color="#b8b5b0" />
       </mesh>
+
+      <primitive position = {[-22, 17, 121.5]} object={lamp.scene} scale = {0.01} />
+      <primitive position = {[12, 17, 124]} object={lamp.scene.clone()} scale = {0.01} />
 
 
  </>
