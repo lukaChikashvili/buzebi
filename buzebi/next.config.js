48f7fc45fff2b53f0c6b4c86bc56dc512a2df5d3
luka chikashvili <lukachikashvili2020@gmail.com>
2026-03-25 22:16:59 +0400

    add next config error

--- buzebi/next.config.js
+++ buzebi/next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      turbo: false
-    },
-  
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        use: 'raw-loader'
-      })
-      return config
-    }
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: 'raw-loader'
+    })
+    return config
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
